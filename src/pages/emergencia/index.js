366b69bd239805a2a0dca6e25bf19d92bce06d1f
Marcos Lima <marcos.henriiquee1@gmail.com>
2021-05-02 17:49:07 -0300

    Retoques finais

--- src/pages/emergencia/index.js
+++ src/pages/emergencia/index.js
@@ -9,9 +9,9 @@ export default class Emergencia extends Component {
             <>
                 <Navbar />
                 <div className="card cardemerg">
-                    <img className="card-img cardemergimg" src="http://biasfortes.com.br/wp-content/uploads/2018/03/dor-de-dente-2.png" alt="Imagem do card" />
+                    <img className="card-img cardemergimg" src="https://imgur.com/jfVvlzU.gif" alt="Imagem do card" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title cardemergtitle"><i className="fas fa-exclamation-circle"></i><br/>Atendimento de Emergência</h5>
+                        <h5 className="card-title cardemergtitle"></h5>
                     </div>
                 </div>
                 <div className="emergtexto">
@@ -19,7 +19,7 @@ export default class Emergencia extends Component {
                     <p></p>Telefone para contato: (61) 9 9334-8464  <br/><br/>            
                     <a href="https://api.whatsapp.com/send?phone=5561993348464&text=Ol%C3%A1" className="btn btn-success btn-lg mb-5 ml-2 wppbtn"><i className="fab fa-whatsapp"></i>WhatsApp</a>
                 </div>
-                <div className="alert alert-danger font-weight-bold text-center mt-5 mb-5" role="alert">
+                <div className="alert alert-danger font-weight-bold text-center mb-5" role="alert">
                     É necessário agendar o atendimento !!! Ligue agora.
                 </div>
                 <Footer />
--- src/pages/sobrenos/index.js
+++ src/pages/sobrenos/index.js
@@ -1,16 +1,19 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 import Sobrecard from '../../Components/Sobrecard';
 
 
 export default class Sobre extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <>
                 <Navbar />
-                <Sobrecard />
-                <Footer />
+                <div class="card fotosobre">
+                    <img class="card-img-bottom fotosobre" src="https://imgur.com/veP37Vd.jpg" alt="Imagem de capa do card" />
+                </div>
+                    <Sobrecard />
+                    <Footer />
             </>
         )
     }
